c83c4911aa3c8b1fb8fad170faf30ecc75b36444
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-03-16 01:12:18 +0700

    add color pallete & text style

---
+++ lib/const/color_palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette{
+  static const primary = Color(0xff124A3F);
+  static const secondary = Color(0xff156E5C);
+  static const body = Color(0xff222222);
+  static const lines = Color(0xffCCCCCC);
+  static const white = Color(0xffFFFFFF);
+  static const button = Color(0xff1328DE);
+}
---
+++ lib/const/text_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'color_palette.dart';
+
+TextStyle title = GoogleFonts.karla(color: ColorPalette.body, fontSize: 20, fontWeight: FontWeight.w700);
+TextStyle subTitle = GoogleFonts.karla(color: ColorPalette.body, fontSize: 16, fontWeight: FontWeight.w400);
